e5908d1de4919a4d333cf8bedba5a36ed725c926
CasperDkk <daniel.kamau8@gmail.com>
2025-04-25 16:53:17 +0300

    07 corrected

--- submission/07.sh
+++ submission/07.sh
@@ -1,7 +1,12 @@
 # what is the coinbase tx in this block 243,834
 
+
 COINBASE_BLOCK=243834
 
-COINBASE_TX=bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash "$COINBASE_BLOCK") | jq -r '.tx[0]'
+# Get the block hash for the coinbase block
+COINBASE_BLOCK_HASH=$(bitcoin-cli -signet getblockhash "$COINBASE_BLOCK")
+
+# Get the txid of the coinbase transaction
+COINBASE_TX=$(bitcoin-cli -signet getblock "$COINBASE_BLOCK_HASH" | jq -r '.tx[0]')
 
 echo $COINBASE_TX
